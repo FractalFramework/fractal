1e291934117955fdb0b0792ad329a68d5110b235
agence2dav <agence2dav@gmail.com>
2026-01-30 13:39:39 +0100

    260130

--- cnfg/site.com.php
+++ cnfg/site.com.php
@@ -1,10 +1,17 @@
 <?php
-$s=['localhost','root','database','password'];//connexion to database
+/*
+ * Renname this file as yoursite.com.php
+*/
+
+#connexion to database
+$s=['localhost','root','database','password'];
+
+#Config
 $r=[
 'site'=>'mySite',//name of your site
 'index'=>'home',//App to open
 'noadmin'=>0,//not display admin to visitors
-'usrboot'=>'',//
+'usrboot'=>'',//name of default usr, in case you're using App "telex" and want to make an homepage with your own account
 'srv'=>'ffw.ovh',//server of updates
 'favicon'=>1,//favicon of your site
 'tz'=>'Europe/Paris',//timezone
