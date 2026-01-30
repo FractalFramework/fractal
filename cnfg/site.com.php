e1e3e19ea19e28825b3f2b9e7415dfae0d8c7321
agence2dav <agence2dav@gmail.com>
2026-01-30 13:35:02 +0100

    260130

--- cnfg/site.com.php
+++ cnfg/site.com.php
@@ -1,4 +1,13 @@
 <?php
-$s=['localhost','root','database','password'];
-$r=['site'=>'Site','index'=>'mac','noadmin'=>0,'usrboot'=>'','srv'=>'ffw.ovh','favicon'=>1,'tz'=>'Europe/Paris','lang'=>'fr'];
+$s=['localhost','root','database','password'];//connexion to database
+$r=[
+'site'=>'mySite',//name of your site
+'index'=>'home',//App to open
+'noadmin'=>0,//not display admin to visitors
+'usrboot'=>'',//
+'srv'=>'ffw.ovh',//server of updates
+'favicon'=>1,//favicon of your site
+'tz'=>'Europe/Paris',//timezone
+'lang'=>'fr'//default lang, given after get lang of navigator
+];
 ?>
