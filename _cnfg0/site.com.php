528a5577c56a73b529e08edfd58eb5113967434e
agence2dav <agence2dav@gmail.com>
2026-01-30 13:28:42 +0100

    fixed untracked files

--- _cnfg0/site.com.php
+++
@@ -1,4 +0,0 @@
-<?php
-$s=['localhost','root','database','password'];
-$r=['site'=>'Site','index'=>'mac','noadmin'=>0,'usrboot'=>'','srv'=>'ffw.ovh','favicon'=>1,'tz'=>'Europe/Paris','lang'=>'fr'];
-?>
